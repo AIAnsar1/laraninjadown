8f563c623455b58e096ddf1f69967dd5e69a3d94
Ansar <Wattaban@proton.me>
2025-07-13 22:53:28 +0500

    updating

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-    App\Providers\ApiResponseServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Barryvdh\Debugbar\ServiceProvider::class,
